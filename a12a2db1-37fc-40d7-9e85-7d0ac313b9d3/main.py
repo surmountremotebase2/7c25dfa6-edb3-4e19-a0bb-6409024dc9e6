ea439e22b3aa81b12776b9f59e8ac1bee996b862
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-15 23:33:28 -0400

    User Justin Graves saved changes to multiple files files at 03/16/2024, 03:33:28 UTC time.

--- a12a2db1-37fc-40d7-9e85-7d0ac313b9d3/main.py
+++ a12a2db1-37fc-40d7-9e85-7d0ac313b9d3/main.py
@@ -48,16 +48,16 @@ class TradingStrategy(Strategy):
             allocation_dict = {"SPXS": 0.0}
             #log("Upward trend")
             if spxl_delta < spy_delta * 1.05:
-                allocation_dict = {"SPXL": 1.0}
-            else:
                 allocation_dict = {"SPXL": 0.0}
+            else:
+                allocation_dict = {"SPXL": 1.0}
         elif upward_trend < downward_trend:
             #log("downward trend")
             allocation_dict = {"SPXL": 0.0}
             if spxs_delta < abs(spy_delta * 1.05):
-                allocation_dict = {"SPXS": 1.0}
-            else:
                 allocation_dict = {"SPXS": 0.0}
+            else:
+                allocation_dict = {"SPXS": 1.0}
         else:
             #log("In the else")
             return TargetAllocation({})
