e1856cf5b854c6dad07899ded874e7bdde577e71
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-21 23:37:22 -0400

    User Justin Graves saved changes to multiple files files at 03/22/2024, 03:37:21 UTC time.

--- 427bc0e8-ce10-4bda-9b47-d9c866b03042/main.py
+++ 427bc0e8-ce10-4bda-9b47-d9c866b03042/main.py
@@ -20,9 +20,9 @@ class TradingStrategy(Strategy):
         if not sma_VIX or len(sma_VIX) < 5:
             return TargetAllocation({})
 
-        macdh_SPY = macd_SPY[1]
+        macdh_SPY = macd_SPY['MACDh_12_26_9']
 
-        print("Value is: " + str(macdh_SPY['MACDh_12_26_9']))
+        print("Value is: " + str(macdh_SPY[-1]))
 
         # Figure out the general trend of SPY
         if macdh_SPY[-1] > 0:
