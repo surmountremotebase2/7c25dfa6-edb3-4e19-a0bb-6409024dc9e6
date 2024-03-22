be3a5e1e5655f6ce08c9c0f1b6967024ce8f1403
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-22 00:16:30 -0400

    User Justin Graves saved changes to multiple files files at 03/22/2024, 04:16:29 UTC time.

--- 427bc0e8-ce10-4bda-9b47-d9c866b03042/main.py
+++ 427bc0e8-ce10-4bda-9b47-d9c866b03042/main.py
@@ -34,7 +34,7 @@ class TradingStrategy(Strategy):
             else: 
                 allocation_dict = {"SPXS": 0, "SPXL": 0}'''
         
-        if macdh_SPY[-1] > 2:
+        if macdh_SPY[-1] > 1:
             allocation_dict = {"SPXS": 100, "SPXL": 0}
         elif macdh_SPY[-1] < -0.1:
             allocation_dict = {"SPXL": 100, "SPXS": 0}
