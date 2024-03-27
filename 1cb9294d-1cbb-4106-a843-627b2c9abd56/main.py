47e903c438dfb70bc94494bf3a22aab90931fa83
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-26 21:34:37 -0400

    User Justin Graves saved changes to multiple files files at 03/27/2024, 01:34:36 UTC time.

--- 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
+++ 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
@@ -37,7 +37,7 @@ class TradingStrategy(Strategy):
 
         if upward_trend > downward_trend: # Go in on long
             if macdh_SPY[-1] < -1.68:
-                allocation_dict = {"SPXS": 70, "SPXL": 30}
+                allocation_dict = {"SPXS": 80, "SPXL": 20}
             else:
                 if rsi_SPY[-1] < 62: 
                     allocation_dict = {"SPXS": 20, "SPXL": 80}
