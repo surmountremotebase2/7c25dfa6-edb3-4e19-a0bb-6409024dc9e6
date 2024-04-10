62eeb80e8fe57f2d1b75f47c564fd58707d55111
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-10 14:31:15 -0400

    User Justin Graves saved changes to multiple files files at 04/10/2024, 18:31:14 UTC time.

--- 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
+++ 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
@@ -1,5 +1,5 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import SMA, MACD, RSI  # We'll use the Simple Moving Average (SMA)
+from surmount.technical_indicators import SMA, MACD, RSI, EMA  # We'll use the Simple Moving Average (SMA)
 from surmount.logging import log
 from surmount.data import Asset
 
