1187af0da4c5f69c9590f759d6a49a08caa49fd1
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-10 16:56:10 -0400

    User Justin Graves saved changes to multiple files files at 04/10/2024, 20:56:09 UTC time.

--- 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
+++ 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
@@ -11,7 +11,7 @@ class TradingStrategy(Strategy):
 
     @property
     def interval(self):
-        return "1day"
+        return "4hour"
 
     def run(self, data):
         macd_SPY = MACD("SPY", data["ohlcv"], 5, 10)
