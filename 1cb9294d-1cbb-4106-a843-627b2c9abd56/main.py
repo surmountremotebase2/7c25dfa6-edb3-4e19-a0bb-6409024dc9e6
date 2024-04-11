44cffdf609f262187d0c54a81cb22120f1576bc7
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-10 23:51:29 -0400

    User Justin Graves saved changes to multiple files files at 04/11/2024, 03:51:28 UTC time.

--- 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
+++ 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
@@ -24,8 +24,8 @@ class TradingStrategy(Strategy):
 
         #macdh_SPY = macd_SPY['MACDh_5_10_9']
 
-        short_sma_SPY = SMA("SPY", data['ohlcv'], length=3)
-        short_ema_SPY = EMA("SPY", data['ohlcv'], length=3)
+        short_sma_SPY = SMA("SPY", data['ohlcv'], length=5)
+        short_ema_SPY = EMA("SPY", data['ohlcv'], length=5)
 
         if short_ema_SPY[-1] > (short_sma_SPY[-1] * 1.02):
             # Bull Market
