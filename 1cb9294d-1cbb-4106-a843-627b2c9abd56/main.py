43805c8ddc5c5ecca606b28976ba2b38c04329fa
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-10 23:31:17 -0400

    User Justin Graves saved changes to multiple files files at 04/11/2024, 03:31:17 UTC time.

--- 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
+++ 1cb9294d-1cbb-4106-a843-627b2c9abd56/main.py
@@ -14,46 +14,16 @@ class TradingStrategy(Strategy):
         return "1hour"
 
     def run(self, data):
-        macd_SPY = MACD("SPY", data["ohlcv"], 5, 10)
+        #macd_SPY = MACD("SPY", data["ohlcv"], 5, 10)
 
-        sma_SPXL = SMA("SPXL", data["ohlcv"], length=5)
-        sma_SPY = SMA("SPY", data["ohlcv"], length=5)
-        sma_SPXS = SMA("SPXS", data["ohlcv"], length=5)
-
-        rsi_SPY = RSI("SPY", data["ohlcv"], length=14)
-        rsi_SPXL = RSI("SPXL", data["ohlcv"], length=14)
-
-        spxl_delta = (sma_SPXL[-1] - sma_SPXL[-2]) / sma_SPXL[-1]
-        spy_delta = (sma_SPY[-1] - sma_SPY[-2]) / sma_SPY[-1]
-        spxs_delta = (sma_SPXS[-1] - sma_SPXS[-2]) / sma_SPXS[-1]
-
-        spy_recents = sma_SPY[-15:]
-        spy_differences = [spy_recents[i+1] - spy_recents[i] for i in range(len(spy_recents)-1)]
+        #spy_recents = sma_SPY[-15:]
+        #spy_differences = [spy_recents[i+1] - spy_recents[i] for i in range(len(spy_recents)-1)]
 
         
-        upward_trend = sum(d > 0 for d in spy_differences)
-        downward_trend = sum(d < 0 for d in spy_differences)
+        #upward_trend = sum(d > 0 for d in spy_differences)
+        #downward_trend = sum(d < 0 for d in spy_differences)
 
-        macdh_SPY = macd_SPY['MACDh_5_10_9']
-
-        '''if upward_trend > downward_trend: # Go in on long
-            if macdh_SPY[-1] < -1.68:
-                allocation_dict = {"SPXS": 70, "SPXL": 30}
-            else:
-                if rsi_SPY[-1] < 62: 
-                    allocation_dict = {"SPXS": 10, "SPXL": 90}
-                else:
-                    allocation_dict = {"SPXL": 0, "SPXS": 0}
-        elif upward_trend < downward_trend:
-            if macdh_SPY[-1] < -1.68:
-                allocation_dict = {"SPXS": 100, "SPXL": 0}
-            else:
-                if rsi_SPY[-1] < 62: 
-                    allocation_dict = {"SPXS": 0, "SPXL": 100}
-                else:
-                    allocation_dict = {"SPXL": 0, "SPXS": 0}
-        else:
-            return TargetAllocation({})'''
+        #macdh_SPY = macd_SPY['MACDh_5_10_9']
 
         short_sma_SPY = SMA("SPY", data['ohlcv'], length=5)
         short_ema_SPY = EMA("SPY", data['ohlcv'], length=5)
@@ -65,34 +35,4 @@ class TradingStrategy(Strategy):
             # Bear Market
             allocation_dict = {"SPXL": 0, "SPXS": 100}
 
-        '''if data['ohlcv'][-1]['SPY']['close'] > (short_ema_SPY[-1] * 1.02):
-            # Above our short SMA with buffer - upward trajectory
-            logging_value = "Upward Potential on: " + data['ohlcv'][-1]['SPY']['date']
-            log(logging_value)
-            if rsi_SPY[-1] < 62:
-                allocation_dict = {"SPXS": 10, "SPXL": 90}
-            else:
-                allocation_dict = {"SPXL": 0, "SPXS": 0}
-        elif data['ohlcv'][-1]['SPY']['close'] < (short_sma_SPY[-1] * 1.02):
-            # Below our short SMA with buffer - downward trajectory
-            if macdh_SPY[-1] < -1.68:
-                allocation_dict = {"SPXS": 100, "SPXL": 0}
-            else:
-                allocation_dict = {"SPXL": 0, "SPXS": 0}
-        else:
-            # About even - we don't really care.
-            return TargetAllocation({}) '''
-
         return TargetAllocation(allocation_dict)
-
-
-
-        '''elif upward_trend < downward_trend:
-            # Go in on short
-            if macdh_SPY[-1] < -1.68:
-                allocation_dict = {"SPXS": 90, "SPXL": 10}
-            else:
-                if rsi_SPY[-1] < 62: 
-                    allocation_dict = {"SPXS": 30, "SPXL": 70}
-                else:
-                    allocation_dict = {"SPXL": 0, "SPXS": 0}'''
