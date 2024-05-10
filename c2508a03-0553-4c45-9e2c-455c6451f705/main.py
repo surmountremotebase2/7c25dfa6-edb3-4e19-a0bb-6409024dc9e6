6368638850963f13ca1eabe306565c65a9bcfcc4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-05-10 17:24:44 -0400

    User Justin Graves saved changes to multiple files files at 05/10/2024, 21:24:44 UTC time.

--- c2508a03-0553-4c45-9e2c-455c6451f705/main.py
+++ c2508a03-0553-4c45-9e2c-455c6451f705/main.py
@@ -1,1 +1,55 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+   @property
+   def assets(self):
+      return ["SPY", "QQQ"]
+
+   @property
+   def interval(self):
+      return "1day"
+
+   def run(self, data):
+      holdings = data["holdings"]
+      data = data["ohlcv"]
+      spy_stake = 0
+      qqq_stake = 0
+
+      spy_ma = SMA("SPY", data, 5)
+      qqq_ma = SMA("QQQ", data, 5)
+
+      if len(data)<5:
+         return TargetAllocation({})
+    
+      if not bool(holdings):
+          reutn TargetAllocation({}) 
+
+      current_price_spy = data[-1]["SPY"]['close']
+      current_price_qqq = data[-1]["QQQ"]['close']
+
+      if current_price_spy < spy_ma[-1]:
+         log("going long spy")
+         if holdings["SPY"] >= 0:
+            spy_stake = min(1, holdings["SPY"]+0.1)
+         else:
+            spy_stake = 0.4
+      elif current_price_spy > spy_ma[-1]:
+         log("selling spy")
+         if holdings["SPY"] > 0:
+            spy_stake = 0
+
+      if current_price_qqq < qqq_ma[-1]:
+         log("going long qqq")
+         if holdings["QQQ"] >= 0:
+            qqq_stake = min(1, holdings["QQQ"]+0.1)
+         else:
+            qqq_stake = 0.4
+      elif current_price_qqq > qqq_ma[-1]:
+         log("selling qqq")
+         if holdings["QQQ"] > 0:
+            qqq_stake = 0
+
+      return TargetAllocation({"SPY": spy_stake, "QQQ": qqq_stake})
