53cf1fd279d9060061afb5470f82c8a056d7370a
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 21:37:36 -0400

    User Justin Graves saved changes to multiple files files at 06/04/2024, 01:37:35 UTC time.

--- 525fda3d-4c8b-4c30-a8b9-08c1a187b020/main.py
+++ 525fda3d-4c8b-4c30-a8b9-08c1a187b020/main.py
@@ -1,1 +1,22 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA
+from surmount.logging import log
+import pandas as pd 
+import numpy as np 
+from surmount.data import InsiderTrading
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        # Define the assets to be used in the strategy
+        self.data_list = [InsiderTrading("AAPL")]
+        return ["AAPL"]
+
+    @property
+    def interval(self):
+        # The data interval desired for the strategy. Daily in this case.
+        return "4hour"
+
+    def run(self, data):
+        insider_trading_dict = data[("insider_trading", "AAPL")]
+        print(insider_trading)
