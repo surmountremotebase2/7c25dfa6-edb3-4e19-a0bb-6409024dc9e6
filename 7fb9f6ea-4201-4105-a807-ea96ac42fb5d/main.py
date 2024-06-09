70835d742cf15627384d8cf28d638c5a7f82363b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 11:17:31 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 15:17:31 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -6,16 +6,14 @@ class TradingStrategy(Strategy):
 
     def __init__(self):
         self.tickers = [
-            "NEE", "TSLA", "ENPH", "BEP", "FSLR", "ORA", "TPIC", "RUN", "SEDG",
-            "AWK", "WM", "ECL", "XYL", "ITRI", "BLDP", "CLH", "BE", "FCEL",
-            "PLUG", "AEIS", "DAR", "AMRC", "HASI", "AY",
-            "CWEN", "GPRE"
+            "NEE", "TSLA", "ENPH", "BEP", "FSLR", "ORA", "TPIC", "RUN", "SEDG", "AWK",
+            "WM", "ECL", "XYL", "ITRI", "BLDP", "CLH", "CVA", "BE", "FCEL", "PLUG",
+            "AEIS", "DAR", "WTRG", "AMRC", "REGI", "HASI", "PEGX", "AY", "CWEN", "GPRE"
         ]
         self.weights = [
-            0.075, 0.075, 0.075, 0.044, 0.044, 0.044, 0.044, 0.044, 0.044,
-            0.044, 0.044, 0.044, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033,
-            0.032, 0.032, 0.032, 0.032, 0.032, 0.032,
-            0.032, 0.032
+            0.067, 0.067, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033,
+            0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033,
+            0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033, 0.033
         ]
         self.counter = 0
 
@@ -29,22 +27,22 @@ class TradingStrategy(Strategy):
 
     def run(self, data):
         if len(data['ohlcv']) < 2:
-           self.counter += 1
-           if self.counter >= 30:
-               # Normalize the weights to add up to 1
-                total_weight = sum(self.weights)
-                allocation_dict = {self.tickers[i]: self.weights[i]/total_weight for i in range(len(self.tickers))}
+            self.counter += 1
+            if self.counter >= 30:
                 self.counter = 0
+                # Normalize the weights to add up to 1
+                total_weight = sum(self.weights)
+                allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(len(self.tickers))}
                 return TargetAllocation(allocation_dict)
-           else:
-               return None
+            else:
+                return None
 
         today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
         yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
         
-        if today.day == 16 or (today.day > 16 and yesterday.day < 16):
+        if today.day == 15 or (today.day > 15 and yesterday.day < 15):
             # Normalize the weights to add up to 1
             total_weight = sum(self.weights)
-            allocation_dict = {self.tickers[i]: self.weights[i]/total_weight for i in range(len(self.tickers))}
+            allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(len(self.tickers))}
             return TargetAllocation(allocation_dict)
         return None
