bc1211306e978a5bd07d047a2bb53c4eb8902917
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 11:22:24 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 15:22:23 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -30,17 +30,21 @@ class TradingStrategy(Strategy):
     def run(self, data):
         if len(data['ohlcv']) < 2:
             self.counter += 1
-            if self.counter >= 30: 
+            if self.counter >= 30:
                 self.counter = 0
-                allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+                # Normalize the weights to add up to 1
+                total_weight = sum(self.weights)
+                allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(len(self.tickers))}
                 return TargetAllocation(allocation_dict)
             else:
                 return None
-                
+
         today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
         yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
         
-        if today.day == 12 or (today.day > 12 and yesterday.day < 12):
-            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+        if today.day == 31 or (today.day > 31 and yesterday.day < 31):
+            # Normalize the weights to add up to 1
+            total_weight = sum(self.weights)
+            allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(len(self.tickers))}
             return TargetAllocation(allocation_dict)
         return None
