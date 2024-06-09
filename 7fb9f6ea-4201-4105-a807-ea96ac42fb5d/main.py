63f0f7f988a37d0c66068d48d8c862ffd0b603b8
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 12:04:03 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 16:04:02 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -33,7 +33,6 @@ class TradingStrategy(Strategy):
              return None
 
       # Iterate over tickers to find one with data
-      print(str(data['ohlcv'][-1]))
       today_data, yesterday_data = None, None
       for ticker_data in data['ohlcv'][-2]:
          if data['ohlcv'][-2][ticker_data]['date']:
