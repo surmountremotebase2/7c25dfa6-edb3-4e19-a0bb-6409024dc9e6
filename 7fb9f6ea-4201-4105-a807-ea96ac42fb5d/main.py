fefd9f1d26fd5eade9a970437bef0f6ee981a40d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 12:08:59 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 16:08:58 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -4,55 +4,49 @@ from datetime import datetime
 
 class TradingStrategy(Strategy):
 
-   def __init__(self):
-      self.tickers = ["IBM", "GOOGL", "MSFT", "INTC", "HON", "NVDA", "RGTI", "BABA", "DELL", "ANET", "SMCI", "WDC"]
-      self.weights = [0.1, 0.10, 0.10, 0.05, 0.05, 0.10, .05, .10, .10, .05, .10, .05]
-      self.equal_weighting = False
-      self.counter = 0
+    def __init__(self):
+        self.tickers = [
+            "GM", "BBY", "ADP", "BBWI", "WBA", "HSY", "VTR", "ORCL", "LNC", "TPR",
+            "FIS", "C", "NDAQ", "DUK", "PGR", "AWK", "OXY", "BEN", "LUMN", "VRTX",
+            "CDW", "CNC", "CVS", "OTIS", "GD", "REG", "PH", "ZTS", "PCG", "CLX",
+            "ROST", "CMI", "CE", "AEP", "AMD", "ACN", "UPS", "CDAY", "ANET", "NOC"
+        ]
+        self.weights = [
+            0.04, 0.02, 0.03, 0.02, 0.03, 0.03, 0.02, 0.04, 0.02, 0.02,
+            0.03, 0.03, 0.02, 0.03, 0.03, 0.02, 0.02, 0.02, 0.01, 0.02,
+            0.02, 0.02, 0.03, 0.02, 0.03, 0.01, 0.02, 0.03, 0.01, 0.03,
+            0.02, 0.02, 0.02, 0.03, 0.03, 0.03, 0.03, 0.02, 0.01, 0.03,
+            0.01, 0.03
+        ]
+        self.counter = 0
 
-   @property
-   def interval(self):
-      return "1day"
+    @property
+    def interval(self):
+        return "1day"
 
-   @property
-   def assets(self):
-      return self.tickers
+    @property
+    def assets(self):
+        return self.tickers
 
-   def run(self, data):
-      # Ensure there is at least one data point
-      if len(data['ohlcv']) < 1:
-         self.counter += 1
-         if self.counter >= 30:
-            self.counter = 0
-            if self.equal_weighting: 
-               allocation_dict = {I: 1/len(self.tickers) for I in self.tickers}
-            else:
-               allocation_dict = {self.tickers[I]: self.weights[I] for I in range(len(self.tickers))} 
+    def run(self, data):
+        if len(data['ohlcv']) < 2:
+           self.counter += 1
+           if self.counter >= 30:
+              self.counter = 0
+              # Normalize the weights to add up to 1
+              total_weight = sum(self.weights)
+              allocation_dict = {self.tickers[i]: self.weights[i]/total_weight for i in range(len(self.tickers))}
+              return TargetAllocation(allocation_dict)
+           else:
+              return None
+
+
+        today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        
+        if today.day == 17 or (today.day > 17 and yesterday.day < 17):
+            # Normalize the weights to add up to 1
+            total_weight = sum(self.weights)
+            allocation_dict = {self.tickers[i]: self.weights[i]/total_weight for i in range(len(self.tickers))}
             return TargetAllocation(allocation_dict)
-         else:
-             return None
-
-      # Iterate over tickers to find one with data
-      today_data, yesterday_data = None, None
-      for ticker_data in data['ohlcv'][-2]:
-         if data['ohlcv'][-2][ticker_data]['date']:
-            today_data = data['ohlcv'][-1][ticker_data]['date']
-            yesterday_data = data['ohlcv'][-2][ticker_data]['date']
-            break
-
-      # If no data found for any tickers, return None
-      if not today_data or not yesterday_data:
-         return None
-
-      # Parse dates
-      today = datetime.strptime(today_data, '%Y-%m-%d %H:%M:%S')
-      yesterday = datetime.strptime(yesterday_data, '%Y-%m-%d %H:%M:%S')
-      
-      if today.day == 17 or (today.day > 17 and yesterday.day < 17):
-         if self.equal_weighting: 
-            allocation_dict = {I: 1/len(self.tickers) for I in self.tickers}
-         else:
-            allocation_dict = {self.tickers[I]: self.weights[I] for I in range(len(self.tickers))}
-         return TargetAllocation(allocation_dict)
-      
-      return None
+        return None
