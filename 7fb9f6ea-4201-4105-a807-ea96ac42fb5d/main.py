62af5ead6ce63521d6180b34d50c3337931b1715
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 12:15:32 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 16:15:31 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -8,6 +8,7 @@ class TradingStrategy(Strategy):
       self.tickers = ["NVDA", "GOOGL", "MSFT", "AMZN", "TSLA", "IBM", "INTC", "BIDU", "AMD", "CRM", "NOW", "TWLO", "SPLK", "PATH", "CGNX", "MU", "ASML", "DOCU", "CRWD", "OKTA", "PLTR", "ZS", "FSLY", "SNOW", "DDOG", "SNPS", "CDNS", "ANSS", "ADSK", "NTNX", "APPN", "ANET", "TDC", "PEGA", "VRNS", "AI", "ESTC", "TENB"]
       self.weights = [0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.04, 0.03, 0.04, 0.02, 0.02, 0.02, 0.03, 0.02, 0.02, 0.04, 0.02, 0.03, 0.02, 0.01, 0.01, 0.02, 0.02, 0.03, 0.01, 0.01, 0.01, 0.01, 0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.06]
       self.equal_weighting = False
+      self.counter = 0
 
    @property
    def interval(self):
@@ -18,6 +19,18 @@ class TradingStrategy(Strategy):
       return self.tickers
 
    def run(self, data):
+      if len(data['ohlcv']) < 2:
+          self.counter += 1
+          if self.counter >= 30:
+              self.counter = 0
+              if self.equal_weighting: 
+                allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+              else:
+                allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+              return TargetAllocation(allocation_dict)
+          else:
+              return None
+
       today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
       yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
       
