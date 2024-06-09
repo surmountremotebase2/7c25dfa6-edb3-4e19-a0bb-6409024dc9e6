3c29477ff2fb9b43edd7af3904b25e9082d732d3
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-09 12:14:13 -0400

    User Justin Graves saved changes to multiple files files at 06/09/2024, 16:14:12 UTC time.

--- 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
+++ 7fb9f6ea-4201-4105-a807-ea96ac42fb5d/main.py
@@ -4,45 +4,27 @@ from datetime import datetime
 
 class TradingStrategy(Strategy):
 
-    def __init__(self):
-        self.tickers = [
-            "CMI", "ETN", "HON", "LMT", "NOC", "RMD", "UNP", "WM", "NEE",
-            "AWK", "ECL", "XYL", "ITRI", "PH", "GIS", "WTRG", "VST", "EXC",
-            "NEP", "ENPH", "RUN", "ORA", "FSLR", "SEDG", "BE", "AY", "HASI",
-            "REG", "CLX", "VZ", "T", "TMUS"
-        ]
-        self.weights = [
-            0.08, 0.08, 0.08, 0.07, 0.07, 0.07, 0.07, 0.06, 0.06,
-            0.05, 0.05, 0.05, 0.04, 0.04, 0.03, 0.03, 0.03, 0.02,
-            0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.01, 0.01,
-            0.01, 0.01, 0.01, 0.01
-        ]
-        self.counter = 0
+   def __init__(self):
+      self.tickers = ["NVDA", "GOOGL", "MSFT", "AMZN", "TSLA", "IBM", "INTC", "BIDU", "AMD", "CRM", "NOW", "TWLO", "SPLK", "PATH", "CGNX", "MU", "ASML", "DOCU", "CRWD", "OKTA", "PLTR", "ZS", "FSLY", "SNOW", "DDOG", "SNPS", "CDNS", "ANSS", "ADSK", "NTNX", "APPN", "ANET", "TDC", "PEGA", "VRNS", "AI", "ESTC", "TENB"]
+      self.weights = [0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.04, 0.03, 0.04, 0.02, 0.02, 0.02, 0.03, 0.02, 0.02, 0.04, 0.02, 0.03, 0.02, 0.01, 0.01, 0.02, 0.02, 0.03, 0.01, 0.01, 0.01, 0.01, 0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.06]
+      self.equal_weighting = False
 
-    @property
-    def interval(self):
-        return "1day"
+   @property
+   def interval(self):
+      return "1day"
 
-    @property
-    def assets(self):
-        return self.tickers
+   @property
+   def assets(self):
+      return self.tickers
 
-    def run(self, data):
-        if len(data['ohlcv']) < 2:
-            self.counter += 1
-            if self.counter >= 30:
-                self.counter = 0
-                total_weight = sum(self.weights)
-                allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(min(len(self.tickers), len(self.weights)))}
-                return TargetAllocation(allocation_dict)
-            else:
-                return None
-
-        today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
-        yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
-        
-        if today.day == 15 or (today.day > 15 and yesterday.day < 15):
-            total_weight = sum(self.weights)
-            allocation_dict = {self.tickers[i]: self.weights[i] / total_weight for i in range(min(len(self.tickers), len(self.weights)))}
-            return TargetAllocation(allocation_dict)
-        return None
+   def run(self, data):
+      today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      
+      if today.day == 11 or (today.day > 11 and yesterday.day < 11):
+         if self.equal_weighting: 
+            allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+         else:
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+         return TargetAllocation(allocation_dict)
+      return None
