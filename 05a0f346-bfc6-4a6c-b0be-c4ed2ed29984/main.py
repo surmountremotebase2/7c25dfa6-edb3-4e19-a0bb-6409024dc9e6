b0e5dfedbe42561c3adf0a9683fdff458bd8f429
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-13 12:12:00 -0400

    User Justin Graves saved changes to multiple files files at 06/13/2024, 16:11:59 UTC time.

--- 05a0f346-bfc6-4a6c-b0be-c4ed2ed29984/main.py
+++ 05a0f346-bfc6-4a6c-b0be-c4ed2ed29984/main.py
@@ -2,6 +2,7 @@ from surmount.base_class import Strategy, TargetAllocation
 from surmount.technical_indicators import SMA, MACD, RSI
 from surmount.logging import log
 from surmount.data import Asset 
+import random 
 
 class TradingStrategy(Strategy):
     @property
@@ -14,6 +15,11 @@ class TradingStrategy(Strategy):
     
     def randomAssets(self):
         init_list = ['SPY', 'GME', 'NVDA', 'MSFT', 'F', 'AAPL', 'GOOG', 'FAZ', 'SPXL', 'SPXS']
+        return_list = []
+        for ticker in init_list:
+            if random() < .5:
+                return_list.append(ticker)
+        return return_list
 
     def run(self, data):
         log(str(data))
