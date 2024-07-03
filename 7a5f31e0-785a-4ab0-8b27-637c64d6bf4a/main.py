818321ac3d36939a6cc00d31c2457ae8164c056f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-03 17:44:03 -0400

    User Justin Graves saved changes to multiple files files at 07/03/2024, 21:44:02 UTC time.

--- 7a5f31e0-785a-4ab0-8b27-637c64d6bf4a/main.py
+++ 7a5f31e0-785a-4ab0-8b27-637c64d6bf4a/main.py
@@ -29,15 +29,46 @@ class TradingStrategy(Strategy):
         is_time = start_time <= native_datetime.time() <= end_time
 
         return is_thursday and is_time
+    
+    def isFridayAfternoon(self, date_string):
+        date_format = "%Y-%m-%d %H:%M:%S"
+        native_datetime = datetime.strptime(date_string, date_format)
+
+        is_friday = native_datetime.weekday() == 4
+
+        start_time = time(2, 30)
+        end_time = time(3, 30)
+        is_time = start_time <= native_datetime.time() <= end_time
+
+        return is_friday and is_time
+    
+    def isMondayAfternoon(self, date_string):
+        date_format = "%Y-%m-%d %H:%M:%S"
+        native_datetime = datetime.strptime(date_string, date_format)
+
+        is_friday = native_datetime.weekday() == 0
+
+        start_time = time(2, 30)
+        end_time = time(3, 30)
+        is_time = start_time <= native_datetime.time() <= end_time
+
+        return is_friday and is_time
 
     def run(self, data):
         # This is the principal method where the strategy logic is defined.
         log(str(data["ohlcv"][-1]["SPY"]['date']))
         if self.isThursdayAfternoon(data["ohlcv"][-1]["SPY"]['date']):
-            log(str(data["ohlcv"][-1]))
+            #log(str(data["ohlcv"][-1]))
+            log("Thursday Afternoon - going short")
             allocation_dict = {"SPXS": 1.0, "SPXL": 0}
+        elif self.isFridayAfternoon(data["ohlcv"][-1]["SPY"]['date']):
+            log("Friday afternoon - going long")
+            allocation_dict = {"SPXS": 0, "SPXL": 1}
+        elif self.IsMondayAfternoon(data["ohlcv"][-1]["SPY"]['date']):
+            log("Monday Afternoon - exiting")
+            allocation_dict = {"SPXS": 0, "SPXL": 0}
         else:
-            allocation_dict = {}
+            allocation_dict = {"SPY": 0}
 
         '''if sma_SPXL[-1] < sma_SPY[-1]:
             #log("SPXL underperforming SPY, buying SPXL.")
