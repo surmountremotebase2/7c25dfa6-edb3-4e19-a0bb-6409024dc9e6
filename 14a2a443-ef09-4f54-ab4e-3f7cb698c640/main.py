788a52d8bfceb8340d9c94454efbe3ea5ce53767
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-31 15:13:13 -0700

    User Justin Graves saved changes to main.py at 07/31/2024, 22:13:12 UTC time.

--- 14a2a443-ef09-4f54-ab4e-3f7cb698c640/main.py
+++ 14a2a443-ef09-4f54-ab4e-3f7cb698c640/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from datetime import datetime
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ["XOM", "CVX", "SLB", "ECL", "APD", "LIN", "OXY", "PSX", "VLO", "PXD", "EOG", "FANG", "CQP", "MPLX", "SRE", "LNG", "WMB", "KMI", "ET"]
+        self.weights = [0.1, 0.1, 0.08, 0.08, 0.08, 0.08, 0.08, 0.07, 0.05, 0.04, 0.04, 0.03, 0.03, 0.02, 0.02, 0.01, 0.01, 0.01, 0.01]
+        self.equal_weighting = False
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    def run(self, data):
+        today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        
+        if today.day == 14 or (today.day > 14 and yesterday.day < 14):
+            if self.equal_weighting:
+                allocation_dict = {i: 1 / len(self.tickers) for i in self.tickers}
+            else:
+                allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+            return TargetAllocation(allocation_dict)
+        return None
