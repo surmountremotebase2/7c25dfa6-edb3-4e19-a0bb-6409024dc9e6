a8a200cbf0fc8bfb0b245679be202c9eace33f46
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-31 15:18:55 -0700

    User Justin Graves saved changes to multiple files files at 07/31/2024, 22:18:54 UTC time.

--- 14a2a443-ef09-4f54-ab4e-3f7cb698c640/main.py
+++ 14a2a443-ef09-4f54-ab4e-3f7cb698c640/main.py
@@ -5,9 +5,18 @@ from datetime import datetime
 class TradingStrategy(Strategy):
 
     def __init__(self):
-        self.tickers = ["XOM", "CVX", "SLB", "ECL", "APD", "LIN", "OXY", "PSX", "VLO", "PXD", "EOG", "FANG", "CQP", "MPLX", "SRE", "LNG", "WMB", "KMI", "ET"]
-        self.weights = [0.1, 0.1, 0.08, 0.08, 0.08, 0.08, 0.08, 0.07, 0.05, 0.04, 0.04, 0.03, 0.03, 0.02, 0.02, 0.01, 0.01, 0.01, 0.01]
-        self.equal_weighting = False
+        self.tickers = [
+            "AMZN", "AAPL", "META", "GOOGL", "MSFT", "NVDA", "PYPL", "SHOP",
+            "SQ", "TSLA", "TTD", "ADBE", "ATVI", "NFLX", "TWLO", "ZM", "ROKU",
+            "PTON", "SNAP", "SE", "SPOT", "PINS", "UBER", "LYFT", "DOCU", "ETSY",
+            "ZG", "W", "CHWY", "CRWD"
+        ]
+        self.weights = [
+            0.045, 0.045, 0.045, 0.045, 0.045, 0.035, 0.035, 0.035,
+            0.035, 0.035, 0.03, 0.03, 0.03, 0.03, 0.025, 0.025, 0.025,
+            0.025, 0.025, 0.02, 0.02, 0.02, 0.02, 0.02, 0.015, 0.015,
+            0.015, 0.015, 0.015, 0.015
+        ]
 
     @property
     def interval(self):
@@ -18,14 +27,10 @@ class TradingStrategy(Strategy):
         return self.tickers
 
     def run(self, data):
-        if data['ohlcv']:
-            today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
-            yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
-            
-            if today.day == 14 or (today.day > 14 and yesterday.day < 14):
-                if self.equal_weighting:
-                    allocation_dict = {i: 1 / len(self.tickers) for i in self.tickers}
-                else:
-                    allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
-                return TargetAllocation(allocation_dict)
+        today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+        
+        if today.day == 7 or (today.day > 7 and yesterday.day < 7):
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+            return TargetAllocation(allocation_dict)
         return None
