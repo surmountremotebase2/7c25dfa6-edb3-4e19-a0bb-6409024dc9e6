38bccd77f5c9992e2e1ee302854725e4b2dae8c7
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-26 22:07:38 +0500

    User Justin Graves saved changes to multiple files files at 09/26/2024, 17:07:37 UTC time.

--- 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
+++ 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
@@ -77,25 +77,25 @@ class TradingStrategy(Strategy):
 
         return TargetAllocation(allocation_dict)'''
 
-        def run(self, data):
-            #allocation_dict = {"BTCUSD": 1.0}
+    def run(self, data):
+        #allocation_dict = {"BTCUSD": 1.0}
 
-            # Calculate the hitorical SMA's for BTCUSD
-            three_sma = SMA("BTCUSD", data["ohlcv"], length=3)
-            five_sma = SMA("BTCUSD", data["ohlcv"], length=5)
-            seven_sma = SMA("BTCUSD", data["ohlcv"], length=7)
-            ten_sma = SMA("BTCUSD", data["ohlcv"], length=10)
+        # Calculate the hitorical SMA's for BTCUSD
+        three_sma = SMA("BTCUSD", data["ohlcv"], length=3)
+        five_sma = SMA("BTCUSD", data["ohlcv"], length=5)
+        seven_sma = SMA("BTCUSD", data["ohlcv"], length=7)
+        ten_sma = SMA("BTCUSD", data["ohlcv"], length=10)
 
-            if three_sma[-1] > three_sma[-2]:
-                if three_sma[-1] > five_sma[-1]:
-                    allocation_dict = {"BTCUSD": 1.0}
-                else:
-                    allocation_dict = {"BTCUSD": 0.0}
+        if three_sma[-1] > three_sma[-2]:
+            if three_sma[-1] > five_sma[-1]:
+                allocation_dict = {"BTCUSD": 1.0}
             else:
                 allocation_dict = {"BTCUSD": 0.0}
-            
-            if not allocation_dict:
-                allocation_dict = TargetAllocation({})
+        else:
+            allocation_dict = {"BTCUSD": 0.0}
+        
+        if not allocation_dict:
+            allocation_dict = TargetAllocation({})
 
-            # Return the target allocation based on our logic
-            return TargetAllocation(allocation_dict)
+        # Return the target allocation based on our logic
+        return TargetAllocation(allocation_dict)
