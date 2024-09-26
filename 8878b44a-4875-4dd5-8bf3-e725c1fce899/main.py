111a1e64adc2d7c428dcfcf8e7e1a5c49d00b565
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-26 21:46:47 +0500

    User Justin Graves saved changes to multiple files files at 09/26/2024, 16:46:46 UTC time.

--- 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
+++ 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
@@ -41,7 +41,7 @@ class TradingStrategy(Strategy):
 
         return TargetAllocation(allocation_dict)'''
     
-    def run(self, data):
+    '''def run(self, data):
         #allocation_dict = {"BTCUSD": 1.0}
         #return TargetAllocation(allocation_dict)
         # Calculate SMAs
@@ -75,6 +75,29 @@ class TradingStrategy(Strategy):
         if exit_signal:
             allocation_dict = {"BTCUSD": 0.0}
 
-        return TargetAllocation(allocation_dict)
+        return TargetAllocation(allocation_dict)'''
+
+        def run(self, data):
+            #allocation_dict = {"BTCUSD": 1.0}
+
+            # Calculate the hitorical SMA's for BTCUSD
+            three_sma = SMA("BTCUSD", data["ohlcv"], length=3)
+            five_sma = SMA("BTCUSD", data["ohlcv"], length=5)
+            seven_sma = SMA("BTCUSD", data["ohlcv"], length=7)
+            ten_sma = SMA("BTCUSD", data["ohlcv"], length=10)
+
+            if three_sma[-1] > three_sma[-2]:
+                if three_sma[-1] > five_sma[-1]:
+                    allocation_dict = {"BTCUSD": 1.0}
+                else:
+                    allocation_dict = {"BTCUSD": 0.0}
+            else:
+                allocation_dict = {"BTCUSD": 0.0}
+            
+            if not allocation_dict:
+                allocation_dict = TargetAllocation({})
+
+            # Return the target allocation based on our logic
+            return TargetAllocation(allocation_dict)
 
         
