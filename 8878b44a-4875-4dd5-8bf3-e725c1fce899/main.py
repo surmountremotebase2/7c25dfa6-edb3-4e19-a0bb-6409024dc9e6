44005346b4a9b0d1b8e6ba4edf1c2a80ad1d6838
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-26 20:44:12 +0500

    User Justin Graves saved changes to multiple files files at 09/26/2024, 15:44:11 UTC time.

--- 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
+++ 8878b44a-4875-4dd5-8bf3-e725c1fce899/main.py
@@ -42,7 +42,7 @@ class TradingStrategy(Strategy):
         return TargetAllocation(allocation_dict)'''
     
     def run(self, data):
-        allocation_dict{"BTCUSD": 1.0}
+        allocation_dict = {"BTCUSD": 1.0}
         return TargetAllocation(allocation_dict)
         # Calculate SMAs
         three_sma = SMA("BTCUSD", data["ohlcv"], length=5)
