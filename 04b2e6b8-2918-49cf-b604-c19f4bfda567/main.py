8791619f4d884b7e7018309f3fdca7f52aed4cdb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-15 13:33:14 -0400

    User Justin Graves saved changes to main.py at 10/15/2024, 17:33:13 UTC time.

--- 04b2e6b8-2918-49cf-b604-c19f4bfda567/main.py
+++ 04b2e6b8-2918-49cf-b604-c19f4bfda567/main.py
@@ -8,12 +8,12 @@ class TradingStrategy(Strategy):
     @property
     def assets(self):
         # Define the assets to be used in the strategy
-        return ["SPXL", "SPY", "SPXS"]
+        return ["SPXL", "SPY", "SPXS", "DPG"]
 
     @property
     def interval(self):
         # The data interval desired for the strategy. Daily in this case.
-        return "4hour"
+        return "1min"
 
     def run(self, data):
         # This is the principal method where the strategy logic is defined.
