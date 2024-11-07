63c0eafeda70f223dc49e72f7a0837f3b29a86c9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-08 01:14:23 +0500

    User Justin Graves saved changes to multiple files files at 11/07/2024, 20:14:22 UTC time.

--- d31ece25-fb22-420a-bfc4-0c945f54a9f9/main.py
+++ d31ece25-fb22-420a-bfc4-0c945f54a9f9/main.py
@@ -1,1 +1,19 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["NVDA"]
+        self.weights = [100]
+
+    @property
+    def interval(self):
+        return "1hour"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    def run(self, data):
+        allocation_dict = {self.tickers[i]: self.weights[i]/sum(self.weights) for i in range(len(self.tickers))}
+        return TargetAllocation(allocation_dict)
