aaa05416c248d46a37b7de97c421af74b928866d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-12-08 08:47:57 +0500

    User Justin Graves saved changes to main.py at 12/08/2024, 03:47:56 UTC time.

--- 18a9ed45-6fbf-44ae-87f0-ed758e70508f/main.py
+++ 18a9ed45-6fbf-44ae-87f0-ed758e70508f/main.py
@@ -1,4 +1,18 @@
-def run(self, data):
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA, MACD, RSI  # We'll use the Simple Moving Average (SMA)
+from surmount.logging import log
+from surmount.data import Asset
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        return ["SPXL", "SPXS", "SPY", "SH"]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
         macd_SPY = MACD("SPY", data["ohlcv"], 5, 10)
         sma_SPXL = SMA("SPXL", data["ohlcv"], length=5)
         sma_SPY = SMA("SPY", data["ohlcv"], length=5)
