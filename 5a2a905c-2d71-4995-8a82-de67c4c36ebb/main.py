f9bdf8659845f4ac7dd1b8f0992d9b07b2f3eadb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-07-31 03:34:21 +0500

    User Justin Graves saved changes to main.py at 07/30/2025, 22:34:21 UTC time.

--- 5a2a905c-2d71-4995-8a82-de67c4c36ebb/main.py
+++ 5a2a905c-2d71-4995-8a82-de67c4c36ebb/main.py
@@ -1,1 +1,34 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA
+from surmount.logging import log
+import pandas as pd 
+import numpy as np 
+
+class TradingStrategy(Strategy):
+    in_trade = 0
+    resistance_found = 0
+
+    
+    @property
+    def assets(self):
+        # Define the assets to be used in the strategy
+        return ["SPXL", "SPY", "SPXS"]
+
+    @property
+    def interval(self):
+        # The data interval desired for the strategy. Daily in this case.
+        return "5min"
+        #return "4hour"
+
+    def run(self, data):
+        # This is the principal method where the strategy logic is defined.
+
+        # Check the opening candle high/low - wait for a rise above, or fall below. 
+        log(data['ohlcv'])
+        
+        
+        if not allocation_dict:
+            allocation_dict = TargetAllocation({})
+
+        # Return the target allocation based on our logic
+        return TargetAllocation(allocation_dict)
